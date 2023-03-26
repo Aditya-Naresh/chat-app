faa2314aee10a9190a7dedf76efefd3669f1613d
Aditya-Naresh <adityanaresh007@gmail.com>
2023-03-26 13:53:54 +0530

    Deployment

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import { ChatEngine} from "react-chat-engine";
 
 import ChatFeed from './components/ChatFeed'
+import LoginForm from "./components/LoginForm";
 
 import './App.css'
 
 const App = () => {
+    if(!localStorage.getItem('username')) return <LoginForm />
+
     return (
         <ChatEngine 
             height ="100vh"
             projectID={"c8b2e343-dfb6-422b-8d2a-6c857d61ba35"}
-            userName={"aditya"}
-            userSecret={"123456"}
+            userName={localStorage.getItem('username')}
+            userSecret={localStorage.getItem('password')}
             renderChatFeed = {(ChatAppProps) => <ChatFeed {...ChatAppProps} /> }
         />
     )
